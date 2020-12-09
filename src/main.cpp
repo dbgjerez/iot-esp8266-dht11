84d4e1f484c5c57c6c7fd4c16b6ba78341fd42ac
David Borrego <dborrego@atsistemas.com>
2020-12-09 21:59:48 +0100

    dht sensor configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,44 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include "DHT.h"
 
+#define DHT_PIN D2
+#define DHTTYPE DHT11
+// Sensor is very slow, readings should be almost every 2 seconds
 #define DELAY 5000
+
+DHT dht(DHT_PIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
+  dht.begin();
 }
 
 void loop() {
   delay(DELAY);
-  Serial.println("Hello world!");
+
+  // Read the humidity
+  float h = dht.readHumidity();
+  // Read temperature as Celsius 
+  float t = dht.readTemperature();
+
+  // Check if any reads have failed
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed reading from DHT sensor"));
+    return;
+  }
+
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+  
+  Serial.print(F("\nHumedity: "));
+  Serial.print(h);
+  Serial.print(F("% "));
+  Serial.print(F("Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(F("Head index: "));
+  Serial.print(hic);
+  Serial.print(F("°C"));
+
 }
